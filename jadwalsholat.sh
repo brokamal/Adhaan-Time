3b8749b29ec12cf11f81738f24541970206dc045
brokamal <kamalbc@keemail.me>
2024-08-08 18:58:14 +0700

    Update jadwalsholat.sh

--- jadwalsholat.sh
+++ jadwalsholat.sh
@@ -6,13 +6,16 @@ LOCATION=$1
 YEAR=$2 
 MONTH=$3 
 DAY=$4
-
+PRAYER=$5
 
 
 URL="https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/$LOCATION/$YEAR/$MONTH.json"
 
 response=$(curl -s $URL) 
 
-echo :"$response"
+adhaan_time=$(echo $response | jq -r --arg date "$YEAR-$MONTH-$DAY" --arg prayer "$PRAYER" '.[] | select(.tanggal == $date) | .[$prayer]')
+
+
+echo "$PRAYER time on $YEAR-$MONTH-$DAY in $LOCATION is : $adhaan_time"
 
 
