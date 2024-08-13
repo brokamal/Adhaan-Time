7442443030631498c88fce5c5f2be6606a5e1cea
brokamal <kamalbc@keemail.me>
2024-08-13 08:55:50 +0700

    add file

---
+++ adhaan-time.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#
+
+LOCATION=$1
+YEAR=$2 
+MONTH=$3 
+DAY=$4
+PRAYER=$5
+
+
+URL="https://raw.githubusercontent.com/lakuapik/jadwalsholatorg/master/adzan/$LOCATION/$YEAR/$MONTH.json"
+
+response=$(curl -s $URL) 
+
+adhaan_time=$(echo $response | jq -r --arg date "$YEAR-$MONTH-$DAY" --arg prayer "$PRAYER" '.[] | select(.tanggal == $date) | .[$prayer]')
+
+
+echo "$PRAYER time is : $adhaan_time"
+
